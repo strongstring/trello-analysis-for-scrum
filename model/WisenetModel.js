c8bdbea493bca35d13874a16e6c47887d555f5f4
strongstring <ss.strongstring@gmail.com>
2017-03-08 14:50:25 +0900

    update link

--- model/WisenetModel.js
+++ model/WisenetModel.js
@@ -6,6 +6,12 @@ WisenetModel.setImageLink("/images/icon_wisenetmobile.png");
 WisenetModel.setPage("wisenet");
 WisenetModel.setVersion([
 	{ 
+		"version" : "1.00.170308",
+		"option"  : "DVR Version",
+		"link" : "https://dl.dropboxusercontent.com/s/uj3336v5xnr4qdu/dvr_test.apk", 
+		"platform" : "android",
+	},
+	{ 
 		"version" : "1.00.170307",
 		"option"  : "Stable",
 		"link" : "https://dl.dropboxusercontent.com/s/k6qvehjxpzdml8s/manifest.plist", 
